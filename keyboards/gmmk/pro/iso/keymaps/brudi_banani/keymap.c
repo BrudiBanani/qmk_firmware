22b10c953a23411cd5b4f77a02ded5560816c09a
Linus Schürtz <post@linus-schuertz.de>
2022-01-26 20:34:50 +0100

    make numpad green fix comment for encoder actions

--- keyboards/gmmk/pro/iso/keymaps/brudi_banani/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/brudi_banani/keymap.c
@@ -21,12 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Ins           Rotary(Mute)
-//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
-//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
-//      Caps     A        S        D        F        G        H        J        K        L        ;        "        #        Enter             PgDn
-//      Sh_L     \        Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
-//      Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
+    //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Ins           Rotary(Mute)
+    //      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
+    //      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
+    //      Caps     A        S        D        F        G        H        J        K        L        ;        "        #        Enter             PgDn
+    //      Sh_L     \        Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
+    //      Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
 
 
     [_BASE] = LAYOUT(
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            KC_BSPC,                            _______, _______, _______, RCTL(KC_LEFT), RCTL(KC_PGDN), RCTL(KC_RIGHT)
     ),
 
-#ifdef COLEMAK_LAYER_ENABLE
+    #ifdef COLEMAK_LAYER_ENABLE
     [_COLEMAK] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,           KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFTCAPSWIN, KC_NUBS, KC_Z, KC_X, KC_C,   KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN1),KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
-#endif // COLEMAK_LAYER_ENABLE
+    #endif // COLEMAK_LAYER_ENABLE
 };
 
 #if defined(ENCODER_ENABLE) && !defined(ENCODER_DEFAULTACTIONS_ENABLE)     // Encoder Functionality when not using userspace defaults
@@ -84,11 +84,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             unregister_mods(MOD_BIT(KC_RSFT));
             encoder_action_navpage(clockwise);
             register_mods(MOD_BIT(KC_RSFT));
-        } else if (mods_state & MOD_BIT(KC_LCTL)) {  // if holding Left Ctrl, navigate next/prev word
+        } else if (mods_state & MOD_BIT(KC_LCTL)) {  // if holding Left Ctrl, change media next/prev track
             encoder_action_mediatrack(clockwise);
         } else if (mods_state & MOD_BIT(KC_RCTL)) {  // if holding Right Ctrl, change rgb hue/colour
             encoder_action_rgbhue(clockwise);
-        } else if (mods_state & MOD_BIT(KC_LALT)) {  // if holding Left Alt, change media next/prev track
+        } else if (mods_state & MOD_BIT(KC_LALT)) {  // if holding Left Alt, navigate next/prev word
             encoder_action_navword(clockwise);
         } else  {
             switch(get_highest_layer(layer_state)) {
@@ -156,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             break;
         case _LOWER:
             for (uint8_t i=0; i<ARRAYSIZE(LED_LIST_NUMPAD); i++) {
-                rgb_matrix_set_color(LED_LIST_NUMPAD[i], RGB_MAGENTA);
+                rgb_matrix_set_color(LED_LIST_NUMPAD[i], RGB_GREEN);
             }
             rgb_matrix_set_color(LED_R4, RGB_MAGENTA);
             rgb_matrix_set_color(LED_R5, RGB_MAGENTA);
